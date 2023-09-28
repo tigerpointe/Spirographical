7fd601489a6089cfac08ca40fd3f5044244a6777
tigerpointe <102432718+tigerpointe@users.noreply.github.com>
2023-09-28 15:12:25 -0400

    Add files via upload

---
+++ get_patriotic.py
@@ -0,0 +1,5 @@
+﻿#!/usr/bin/env python3
+import spirographical as sg
+sg.draw(colors=['red', 'firebrick', 'lightgray',
+                'gray', 'blue', 'mediumblue'],
+        title='Patriotic', twist=-2)
